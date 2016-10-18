d88e7a5e450bbb477cf2a93feedf1000aae61a7f
Tim3tang <Tim3tang@gmail.com>
2016-10-18 15:34:01 +0100

    added account deposit and withdrawal functions + irb tested

--- account.py
+++ account.py
@@ -2,3 +2,16 @@ class Account:
 
     def __init__(self, balance):
         self.balance = balance
+
+        def deposit(self, amount, date):
+            date = date
+            print("You deposited " + str(amount) + "on " + str(date))
+            self.balance = self.balance + amount
+
+        def withdrawal(self, amount, date):
+            date = date
+            print("You withdrew " + str(amount) + "on " + str(date))
+            self.balance = self.balance - amount
+
+        def display(self):
+            return self.balance
