64cb50c5545da83438c44aab25e0a4df84ea98cf
Tim3tang <Tim3tang@gmail.com>
2016-10-18 15:43:15 +0100

    added bank test for history function

--- test_bank.py
+++ test_bank.py
@@ -5,9 +5,16 @@ from bank import Bank
 
 class BankTest(unittest.TestCase):
     def test_bank_is_initially_empty(self):
-        bank = Bank()
+        acc = Account(0)
+        bank = Bank(acc)
         self.assertEqual({}, bank.statement)
         self.assertEqual(len(bank.statement), 0)
+
+    def test_bank_history_updates_statement(self):
+        acc = Account(0)
+        bank = Bank(acc)
+        acc.deposit(10)
+        self.assertIn(acc.deposit, bank.statement)
 
 if __name__ == '__main__':
     unittest.main()
